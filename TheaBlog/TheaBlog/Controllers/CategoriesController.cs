c66d54c2ce81e74d254e92d9cff08ef7c1c6ccd6
gesh4o <gesh4oo@gmail.com>
2016-08-29 00:02:07 +0300

    Categories add.

---
+++ TheaBlog/TheaBlog/Controllers/CategoriesController.cs
@@ -0,0 +1,23 @@
+﻿namespace TheaBlog.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using System.Data.Entity;
+    using TheaBlog.Models;
+
+
+    public class CategoriesController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Categories
+        public ActionResult Index()
+        {
+            var categories = db.Photos.Include(p => p.Image).Include(p => p.Album).GroupBy(p => p.Category).Select(group => new Category { Name = group.Key, Photos = group.ToList() }).ToList();
+            return View(categories);
+        }
+    }
+}
---
+++ TheaBlog/TheaBlog/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace TheaBlog.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    public class Category
+    {
+        public string Name { get; set; }
+
+        public List<Photo> Photos { get; set; }
+    }
+}
