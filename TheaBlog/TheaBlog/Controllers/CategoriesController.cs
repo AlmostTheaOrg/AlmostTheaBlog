3dbfe452295b75eb4394ca2207b3d78b55531795
borko9696 <borko9696@mail.bg>
2016-09-01 01:33:02 +0300

    Add some stuff and functionality to Categories to look good and work better Some graphic changes Added some links

--- TheaBlog/TheaBlog/Controllers/CategoriesController.cs
+++ TheaBlog/TheaBlog/Controllers/CategoriesController.cs
@@ -1,8 +1,11 @@
 ﻿namespace TheaBlog.Controllers
 {
+    using System;
     using System.Linq;
     using System.Web.Mvc;
     using System.Data.Entity;
+    using System.Net;
+
     using TheaBlog.Models;
 
 
@@ -29,6 +32,19 @@
             var categories = this.db.Categories.Include(c => c.Photos).ToList();
 
             return View(categories);
+        }
+
+        public ActionResult Details(string name)
+        {
+
+
+            Category category = db.Categories.Include(c => c.Photos).SingleOrDefault(a => a.Name == name);
+
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
         }
     }
 }
