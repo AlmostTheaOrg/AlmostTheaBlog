8c40370cbeebfb808decebb551aed6304bc5d747
Gesh4o <georgi.stoimenov_96@abv.bg>
2016-09-01 15:30:01 +0300

    Slight views updates.

--- TheaBlog/TheaBlog/Controllers/HomeController.cs
+++ TheaBlog/TheaBlog/Controllers/HomeController.cs
@@ -1,10 +1,7 @@
 ﻿namespace TheaBlog.Controllers
 {
     using Models;
-    using System;
-    using System.Collections.Generic;
     using System.Linq;
-    using System.Web;
     using System.Web.Mvc;
 
     public class HomeController : Controller
